b0b2bb65ef9167173f9291a3038571afb4591b27
czl <czl_289@163.com>
2020-11-23 15:01:47 +0800

    multiroad test version

--- Concurrent_componet/MultiRoad/MultiRoadTest.cpp
+++ Concurrent_componet/MultiRoad/MultiRoadTest.cpp
@@ -12,14 +12,22 @@ static double WRITE_RATIO;
 
 //static char test_str[9] = "deedbeef";
 
-static uint64_t key_demo = 12345678;
+static const uint64_t key_demo = 12345678;
+static const int value_para_num = 10;
+
+struct alignas(128) V_PTR{
+    atomic<uint64_t *>  vp;
+    atomic<bool> write_flag;
+};
 
 struct alignas(128) KV_OBJ{
     uint64_t key;
-    atomic<uint64_t * > value_ptr;
+    V_PTR vps[value_para_num];
+    atomic<int> v_index;
 };
 
-KV_OBJ *kvlist;
+KV_OBJ * kvlist;
+
 bool * conflictlist;
 bool * writelist;
 uint64_t *runtimelist;
@@ -33,6 +41,38 @@ struct alignas(128) R_BUF{
 
 R_BUF * r_bufs;
 
+
+inline void store_value(int tid, int index , uint64_t * v){
+    int ind = index % value_para_num;
+    //find not locked vp
+    while(true){
+        if(!kvlist[index].vps[ind].write_flag.load()){
+            //mutual write
+            bool expected = false;
+            if(kvlist[index].vps[ind].write_flag.compare_exchange_strong(expected,true)){
+                //TODO GC
+                kvlist[index].vps[ind].vp.store(v);
+                kvlist[index].v_index.store(ind);
+                kvlist[index].vps[ind].write_flag.store(false);
+                break;
+            }
+        }
+        ind = (ind + 1) % value_para_num;
+    };
+}
+
+inline void read_value(int tid, int index , uint64_t * v){
+    //read  - v index change
+    int ind = index % value_para_num;
+    while(true){
+        if(!kvlist[index].vps[ind].write_flag.load()){
+            *v = * kvlist[index].vps[ind].vp.load();
+            break;
+        }
+        ind = (ind + 1) % value_para_num;
+    }
+}
+
 void concurrent_worker(int tid){
     Tracer t;
     t.startTime();
@@ -42,15 +82,15 @@ void concurrent_worker(int tid){
             if(writelist[i]){
                 uint64_t * tmp = new uint64_t (i);
                 if(conflictlist[i]){
-                    kvlist[THREAD_NUM].value_ptr.store(tmp);
+                    store_value(tid,THREAD_NUM,tmp);
                 }else{
-                    kvlist[tid].value_ptr.store(tmp);
+                    store_value(tid,tid,tmp);
                 }
             }else{
                 if(conflictlist[i]){
-                    r_bufs[tid].r_buf = * kvlist[THREAD_NUM].value_ptr.load();
+                    read_value(tid,THREAD_NUM,&r_bufs[tid].r_buf);
                 }else{
-                    r_bufs[tid].r_buf = * kvlist[tid].value_ptr.load();
+                    read_value(tid,tid,&r_bufs[tid].r_buf);
                 }
             }
         }
@@ -88,7 +128,12 @@ int main(int argc, char **argv){
     for(size_t i = 0; i < THREAD_NUM + 1; i++) {
         uint64_t * tmp = new uint64_t(0);
         kvlist[i].key = key_demo;
-        kvlist[i].value_ptr.store(tmp, std::memory_order_release);
+        for(size_t j = 0; j < value_para_num; j++){
+            kvlist[i].vps[j].write_flag.store(false);
+            kvlist[i].vps[j].vp = nullptr;
+        }
+        kvlist[i].vps[0].vp = tmp ;
+        kvlist[i].v_index.store(0);
     }
 
     runtimelist = new uint64_t[THREAD_NUM]();
