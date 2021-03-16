07acd02c1b8f496ca3a6a9a0bbdf391559a9de42
ZhiLing Cheng <czl_289@163.com>
2021-03-16 16:55:05 +0800

    add kick

--- Cuckoo_improve/improve_test/new_map.hh
+++ Cuckoo_improve/improve_test/new_map.hh
@@ -1191,6 +1191,7 @@ namespace libcuckoo {
             //do some thing
             size_t par_ptr = buckets_.read_from_bucket_slot(pos.index,pos.slot);
             size_t ptr = get_ptr(par_ptr);
+            if(ptr ==0 ) return false;
             bool a = str_equal_to()(ITEM_KEY(ptr),ITEM_KEY_LEN(ptr),key,key_len);
             ASSERT(a,"key error");
 
